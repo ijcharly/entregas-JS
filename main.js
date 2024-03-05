4e22c04755b1988eb4c3919137c2c97487f3f869
ijcharly <munoz.carlos19@gmail.com>
2024-03-04 18:39:30 -0600

    Primera Entrega 1

--- main.js
+++ main.js
@@ -1,17 +1,11 @@
-// let presupuesto = prompt("Ingresa tu presupuesto:");
-// let precio = prompt("Ingresa El precio del primer producto:");
-// //let precio = prompt("Ingresa El precio del segundo producto:");
-
-// console.log(precio);
-// console.log(presupuesto);
 
 
 function clasificarPorEdad(edad,numero) {
     // Clasificar a la persona en los grupos según su edad
     if (edad <= 18) {
-        console.log("Grupo 1: Menor de 18 años");
+        console.log("Recomendacion #" + numero);
     } else {
-        console.log("Recomendacion: " + numero);
+        console.log("Recomendacion #" + numero);
     }
 }
 
@@ -20,7 +14,7 @@ let edadIngresada = prompt("Ingresa tu Edad:");
 let anuncios = prompt("Cuantas recomendaciones quieres ver:");
 // console.log("edadIngresada " + edadIngresada);
 // console.log("anuncios " + anuncios);
-
+console.log("En base a tu edad de " + edadIngresada + ", te recomendamos lo siguiente :");
 for (let i = 0; i < anuncios; i++) {
     let numeroDeRecomendaciones = i + 1;
     clasificarPorEdad(edadIngresada, numeroDeRecomendaciones);
